0186b7a01958cfea3a0dc564a0aa1130dd6b4743
Ahmed AbdulRahman <ahmedabdulrahman75@riseup.net>
2025-12-30 22:51:17 +0200

    faet: finished with dummy tables

--- column_mapper/column_mapper/doctype/column_mapper/column_mapper.js
+++ column_mapper/column_mapper/doctype/column_mapper/column_mapper.js
@@ -4,7 +4,23 @@
 frappe.ui.form.on("Column Mapper", {
 	refresh(frm) {
 		frm.add_custom_button(__("Import Data"), () => {
-			frm.call("import_data")
+			frm.call("import_data").then(() => {
+				frappe.msgprint({
+					title: __("Notification"),
+					indicator: "green",
+					message: __("Data Imported Successfully"),
+				});
+			});
+			frappe.msgprint({
+				title: __("Notification"),
+				indicator: "green",
+				message: __("Importing in Progress"),
+			});
 		});
+	},
+
+	document_type(frm) {
+		cur_frm.set_value("ref_doc", "");
+		cur_frm.set_value("doc_field", "");
 	},
 });
