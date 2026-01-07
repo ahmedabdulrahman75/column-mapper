dd8873e09d7440fa51fa6f45e481b514c3411d7a
Ahmed AbdulRahman <ahmedabdulrahman75@riseup.net>
2026-01-07 16:55:20 +0200

    fix: femove table

--- column_mapper/column_mapper/doctype/dummy_for_test/test_dummy_for_test.py
+++
@@ -1,9 +0,0 @@
-# Copyright (c) 2025, Ahmed AbdulRahman and Contributors
-# See license.txt
-
-# import frappe
-from frappe.tests.utils import FrappeTestCase
-
-
-class TestDummyForTest(FrappeTestCase):
-	pass
